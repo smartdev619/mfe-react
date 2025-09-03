c78b92c6d2703dcacaa2b6e91ad4218ce79541df
Chhabeg Singh <smartdev619@gmail.com>
2025-09-03 14:54:33 +0530

    add vercel deploy

--- apps/products/src/app/app.tsx
+++ apps/products/src/app/app.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import NxWelcome from './nx-welcome';
-// import {MfeReactComponents} from 'lib/components';
+import { MfeReactComponents } from '@mfe-react/components';
 
 const StyledApp = styled.div`
   // Your style here
@@ -9,7 +9,7 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      {/* <MfeReactComponents /> */}
+      <MfeReactComponents />
       <NxWelcome title="products" />
     </StyledApp>
   );
