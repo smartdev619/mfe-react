bc520cffc3f30e42261f3491e487bcd66a527eea
Chhabeg Singh <smartdev619@gmail.com>
2025-09-03 18:57:25 +0530

    update remote entry

--- apps/shell/module-federation.config.ts
+++ apps/shell/module-federation.config.ts
@@ -1,11 +1,18 @@
 import { ModuleFederationConfig } from '@nx/module-federation';
+const isProd = process.env.NODE_ENV === 'production';
 
 const config: ModuleFederationConfig = {
   name: 'shell',
-  remotes: ['products'],
+  remotes:  [
+      'products',
+      isProd
+        ? 'https://mfe-react-products-ey5jn9ssd-chhabeg-singhs-projects.vercel.app/'
+        : 'http://localhost:4201/',
+    ],
 };
 
 /**
  * Nx requires a default export of the config to allow correct resolution of the module federation graph.
  **/
 export default config;
+
